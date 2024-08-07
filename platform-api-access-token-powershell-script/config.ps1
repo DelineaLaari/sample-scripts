8d659af3965025e6208c51f9f469c878c55703da
DelineaLaari <141368837+DelineaLaari@users.noreply.github.com>
2024-08-06 19:41:14 -0700

    Update config.ps1
    bug fixes

--- platform-api-access-token-powershell-script/config.ps1
+++ platform-api-access-token-powershell-script/config.ps1
@@ -1,8 +1,8 @@
 # Configuration variables
-$TOKEN_URL = "https://your-hostname.delinea.app/identity/api/oauth2/token/xpmplatform"
-$CLIENT_ID = "your-client-id"
-$CLIENT_SECRET = "your-client-secret"
-$SCOPE = "xpmheadless"  
-$GRANT_TYPE = "client_credentials"  # Default grant type
-$REFRESH_GRANT_TYPE = "refresh_token"  # Grant type for refreshing the token
-$TEST_API_URL = "https://your-hostname.delinea.app/identity/entities/xpmusers?detail=true"  # Test API endpoint
+$global:TOKEN_URL = "https://your-hostname.delinea.app/identity/api/oauth2/token/xpmplatform"
+$global:CLIENT_ID = "your-client-id"
+$global:CLIENT_SECRET = "your-client-secret"
+$global:SCOPE = "xpmheadless"  
+$global:GRANT_TYPE = "client_credentials"  # Default grant type
+$global:REFRESH_GRANT_TYPE = "refresh_token"  # Grant type for refreshing the token
+$global:API_URL = "https://your-hostname.delinea.app/identity/entities/xpmusers?detail=true"  # Test API endpoint
